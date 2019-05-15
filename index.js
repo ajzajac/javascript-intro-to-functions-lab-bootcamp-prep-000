cee71b82662adc1bccd1da2b4e32d46b91c2101c
Andrew Zajac <ajzajac@icloud.com>
2019-05-15 14:42:16 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var lowercase = "hello"
+var uppercase = 'HELLO!'
+var mixedCase = 'Hi there!'
+
+
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) { 
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  return 
+  
+}
